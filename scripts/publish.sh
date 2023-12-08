745cd22d85231a59e914449f431dbdda6bd44b6e
Ryan Lewis <accounts+github@ryandlewis.dev>
2023-12-08 12:55:43 -0600

    Add docker automation

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,9 +5,4 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 version=`$SCRIPTPATH/version.sh`
 name=`$SCRIPTPATH/name.sh`
 
-cd $SCRIPTPATH/..
-
-docker build \
-	-t ghcr.io/ad-sdl/${name}:${version} \
-	-t ghcr.io/ad-sdl/${name}:dev \
-	.
+docker push ghcr.io/ad-sdl/${name}:${version}
